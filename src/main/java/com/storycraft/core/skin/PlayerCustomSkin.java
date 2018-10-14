dc2a188a8a46d4f9bcab8ecc770035e3b863f241
storycraft <dbsxoal123@naver.com>
2018-10-14 16:33:08 +0900

    removed debug function

--- src/main/java/com/storycraft/core/skin/PlayerCustomSkin.java
+++ src/main/java/com/storycraft/core/skin/PlayerCustomSkin.java
@@ -2,13 +2,8 @@ package com.storycraft.core.skin;
 
 import com.storycraft.StoryPlugin;
 import com.storycraft.core.MiniPlugin;
-import com.storycraft.server.event.server.ServerUpdateEvent;
 
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 
 public class PlayerCustomSkin extends MiniPlugin implements Listener {
     
@@ -23,11 +18,5 @@ public class PlayerCustomSkin extends MiniPlugin implements Listener {
 
     public void onDisable(boolean reload) {
 
-    }
-
-    @EventHandler
-    public void onUpdate(ServerUpdateEvent e) {
-        Player p = getPlugin().getServer().getPlayer("storycraft");
-        //p.getInventory().addItem(new ItemStack(Material.APPLE));
     }
 }
