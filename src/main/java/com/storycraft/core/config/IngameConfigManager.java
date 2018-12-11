9fdea1f05d6f7349b6ab965ba09b1f2916c4943d
storycraft <dbsxoal123@naver.com>
2018-12-11 20:50:39 +0900

    add command

--- src/main/java/com/storycraft/core/config/IngameConfigManager.java
+++ src/main/java/com/storycraft/core/config/IngameConfigManager.java
@@ -2,26 +2,27 @@ package com.storycraft.core.config;
 
 import java.util.Map.Entry;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
+import com.storycraft.StoryPlugin;
 import com.storycraft.command.ICommand;
 import com.storycraft.config.IConfigEntry;
 import com.storycraft.config.IConfigFile;
-import com.storycraft.config.json.JsonConfigEntry;
 import com.storycraft.config.json.JsonConfigFile;
 import com.storycraft.core.MiniPlugin;
 import com.storycraft.core.rank.ServerRank;
-import com.storycraft.util.GsonTools;
 import com.storycraft.util.MessageUtil;
-import com.storycraft.util.GsonTools.ConflictStrategy;
 import com.storycraft.util.MessageUtil.MessageType;
 
 import org.bukkit.command.CommandSender;
 
 public class IngameConfigManager extends MiniPlugin implements ICommand {
+
+    @Override
+    public void onLoad(StoryPlugin plugin) {
+        plugin.getCommandManager().addCommand(this);
+    }
 
     @Override
     public String[] getAliases() {
