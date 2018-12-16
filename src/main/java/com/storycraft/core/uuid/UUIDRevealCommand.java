6f2b6db2be75a7e5fbf0c9453d240c568ddb575d
storycraft <dbsxoal123@naver.com>
2018-12-16 09:24:40 +0900

    async task mojang request

--- src/main/java/com/storycraft/core/uuid/UUIDRevealCommand.java
+++ src/main/java/com/storycraft/core/uuid/UUIDRevealCommand.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import com.storycraft.StoryPlugin;
 import com.storycraft.command.ICommand;
 import com.storycraft.core.MiniPlugin;
+import com.storycraft.util.AsyncTask;
 import com.storycraft.util.MessageUtil;
 import com.storycraft.util.MojangAPI;
+import com.storycraft.util.AsyncTask.AsyncCallable;
 import com.storycraft.util.MessageUtil.MessageType;
 
 import org.bukkit.ChatColor;
@@ -33,26 +35,33 @@ public class UUIDRevealCommand extends MiniPlugin implements ICommand {
             return;
         }
 
-        OfflinePlayer p = null;
+        new AsyncTask<Void>(new AsyncCallable<Void>() {
+            @Override
+            public Void get() {
+                OfflinePlayer p = null;
 
-        if (args.length > 0) {
-            p = getPlugin().getServer().getOfflinePlayer(args[0]);
+                if (args.length > 0) {
+                    p = getPlugin().getServer().getOfflinePlayer(args[0]);
+        
+                    if (p == null) {
+                        sender.sendMessage(MessageUtil.getPluginMessage(MessageType.FAIL, "UUID", "플레이어 " + args[0] + " 을 찾을 수 없습니다"));
+                        return null;
+                    }
+                }
+                else {
+                    p = (Player) sender;
+                }
 
-            if (p == null) {
-                sender.sendMessage(MessageUtil.getPluginMessage(MessageType.FAIL, "UUID", "플레이어 " + args[0] + " 을 찾을 수 없습니다"));
-                return;
+                sender.sendMessage(MessageUtil.getPluginMessage(MessageType.SUCCESS, "UUID", "플레이어 " + args[0] + " 의 서버 UUID: " + ChatColor.YELLOW + p.getUniqueId().toString()));
+                try {
+                    sender.sendMessage(MessageUtil.getPluginMessage(MessageType.SUCCESS, "UUID", "플레이어  " + args[0] + " 의 계정 UUID: " + ChatColor.YELLOW + MojangAPI.getSessionPlayerUUID(p.getName())));
+                } catch (IOException e) {
+                    sender.sendMessage(MessageUtil.getPluginMessage(MessageType.FAIL, "UUID", "플레이어  " + args[0] + " 의 계정 UUID를 불러 올 수 없습니다"));
+                }
+
+                return null;
             }
-        }
-        else {
-            p = (Player) sender;
-        }
-
-        sender.sendMessage(MessageUtil.getPluginMessage(MessageType.SUCCESS, "UUID", "플레이어 " + args[0] + " 의 서버 UUID: " + ChatColor.YELLOW + p.getUniqueId().toString()));
-        try {
-			sender.sendMessage(MessageUtil.getPluginMessage(MessageType.SUCCESS, "UUID", "플레이어  " + args[0] + " 의 계정 UUID: " + ChatColor.YELLOW + MojangAPI.getSessionPlayerUUID(p.getName())));
-		} catch (IOException e) {
-            sender.sendMessage(MessageUtil.getPluginMessage(MessageType.FAIL, "UUID", "플레이어  " + args[0] + " 의 계정 UUID를 불러 올 수 없습니다"));
-		}
+        }).run();
     }
 
     @Override
