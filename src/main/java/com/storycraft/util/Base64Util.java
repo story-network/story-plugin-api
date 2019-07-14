79c2f3f14e92dde65993d3a9e6352076391c269f
storycraft <storycraft@storyboard.ml>
2019-07-14 13:12:24 +0900

    using java.util

--- src/main/java/com/storycraft/util/Base64Util.java
+++ src/main/java/com/storycraft/util/Base64Util.java
@@ -3,31 +3,16 @@ package com.storycraft.util;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
-import org.bukkit.craftbukkit.libs.org.apache.commons.codec.binary.Base64InputStream;
+import java.util.Base64;
 
 public class Base64Util {
 
     public static byte[] decode(String data) throws IOException {
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            try (Base64InputStream inputStream = new Base64InputStream(new ByteArrayInputStream(data.getBytes()))) {
-                byte[] byteData = new byte[inputStream.available()];
-                
-                inputStream.read(byteData);
-
-                return byteData;
-            }
-        }
+        return Base64.getDecoder().decode(data);
     }
 
-    public static String encode(ByteArrayInputStream data) throws IOException {
-        try (Base64InputStream inputStream = new Base64InputStream(data, true)) {
-            byte[] byteData = new byte[inputStream.available()];
-            
-            inputStream.read(byteData);
-
-            return new String(byteData);
-        }
+    public static String encode(byte[] data) throws IOException {
+        return Base64.getEncoder().encodeToString(data);
     }
 
 }
